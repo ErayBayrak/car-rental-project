b3aebdf4b1f03c717307bb9d8a94bc8f05f5d495
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-03-25 16:19:13 +0300

    Web API project created.

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -20,5 +20,6 @@ namespace Business.Abstract
         IDataResult<List<Car>> GetCarsByBrandId(int id);
         IDataResult<List<Car>> GetCarsByColorId(int id);
         IDataResult<List<CarDetailDto>> GetCarDetails();
+        IDataResult<Car> GetById(int id);
     }
 }

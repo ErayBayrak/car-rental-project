d354f93ad1810463420169aeba8ada03a4dd6adb
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-03-30 15:50:32 +0300

    Aspects,CrossCuttingConcerns and Interceptors files added.

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.ModelYear).GreaterThanOrEqualTo(1886);
+            RuleFor(c => c.ModelYear).LessThanOrEqualTo(2023);
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(0);
+            RuleFor(c => c.Description).MinimumLength(10);
+            RuleFor(c => c.Description).MaximumLength(500);
+        }
+    }
+}
---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace Core.Utilities.Interceptors
+{
+    //Inherited=true => kalıtım yapılan yerde de çalışmasını sağlar
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; }
+
+        public virtual void Intercept(IInvocation invocation)
+        {
+
+        }
+    }
+}

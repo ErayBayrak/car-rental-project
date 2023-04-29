041e4d43aac9bb382de68666dfd9c3ed3cef3c15
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-04-29 23:50:56 +0300

    File helper

--- Business/Abstract/ICarImageService.cs
+++ Business/Abstract/ICarImageService.cs
@@ -15,7 +15,6 @@ namespace Business.Abstract
         IResult Add(IFormFile file, CarImage carImage);
         IResult Delete(CarImage carImage);
         IResult Update(IFormFile file, CarImage carImage);
-
         IDataResult<List<CarImage>> GetAll();
         IDataResult<List<CarImage>> GetByCarId(int carId);
         IDataResult<CarImage> GetByImageId(int imageId);
